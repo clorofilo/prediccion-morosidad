c007d23f4af6588047448065dc69251801d68112
Joan <joan.guillenperez.94@gmail.com>
2025-10-01 12:15:30 +0200

    Create feature.py

--- scr/feature.py
+++ scr/feature.py
@@ -1,1 +1,13 @@
- # Feature engineering
+import pandas as pd
+import os
+
+path_raw_data = os.path.join('data','raw')
+
+def programa_to_id (programa, 
+                    df_programas_id = 
+                    pd.read_excel(os.path.join(
+                        path_raw_data, 'programas-id.xlsx'
+                    ))):
+    
+    id_selected = df_programas_id[df_programas_id['Programa']==programa]['ID PROGRAMA']
+    return int(id_selected.values[0])
